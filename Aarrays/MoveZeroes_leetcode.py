eae52862c1d67031662120152a6999bf867b7315
Sahithi <sahithijallepalli@gmail.com>
2026-03-16 15:31:24 +0530

    "Added Move Zeroes solution (LeetCode)"

---
+++ Aarrays/MoveZeroes_leetcode.py
@@ -0,0 +1,13 @@
+# Problem: Move Zeroes
+# Platform: LeetCode
+# URL: https://leetcode.com/problems/move-zeroes/
+# Difficulty: Easy
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+               if nums[i]==0:
+                temp=nums[i]
+                nums[j],nums[i]=temp,nums[j]
+        return nums

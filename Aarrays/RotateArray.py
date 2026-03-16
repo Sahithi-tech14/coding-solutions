37b1910fb2d232a150ca9f3e2deee57ffc82c509
Sahithi <sahithijallepalli@gmail.com>
2026-03-16 14:45:27 +0530

    Added Python DSA solutions

---
+++ Aarrays/RotateArray.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k=k%len(nums)
+        nums.reverse()
+        nums[:k]=reversed(nums[:k])
+        nums[k:]=reversed(nums[k:])
